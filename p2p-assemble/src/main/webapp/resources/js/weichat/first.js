fdbe55c5fe0a8bace3078d4f61da209c10e3d03b
Ethan.Yuan <windofdusk@gmail.com>
2015-02-27 14:29:27 +0800

    微信首页调整

--- p2p-assemble/src/main/webapp/resources/js/weichat/first.js
+++ p2p-assemble/src/main/webapp/resources/js/weichat/first.js
@@ -1,3 +1,17 @@
 $(function(){
-	$(".nav").find("img").css("width", ($(window).width() - 20) + "px");
+	var images = $(".nav").find("img");
+	var maxWidth = 800;
+	var minWidth = 320;
+	var maxHeight = 141;
+	var windowWidth = $(window).width();
+	var height = maxHeight * (windowWidth / maxWidth);
+	$("#flashBg").css("height", height + "px");
+	$("#flashLine").css("height", height + "px");
+	$("#flash").css("height", height + "px")
+			   .find("a").css("height", height + "px");
+	images.css("width", windowWidth + "px");
+	images.css("height", height + "px");
+	var documentWidth = (windowWidth > maxWidth ? maxWidth : (windowWidth < minWidth ? windowWidth : windowWidth));
+	$(".flash_bar").css("left", ((documentWidth - $(".flash_bar").width()) / 2) + "px")
+				   .css("top", $("#flashBg").height() - 23 + "px");
 });
